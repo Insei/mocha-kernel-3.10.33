755b21e0024d2cadc13d0b3e92c664bd9236d92a
TheVirus! <kingmouryarahul@gmail.com>
2017-10-23 17:02:43 +0530

    Update card.c

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -85,11 +85,8 @@ static int nrpacks = 8;		/* max. number of packets per urb */
 static int device_setup[SNDRV_CARDS]; /* device parameter for this card */
 static bool ignore_ctl_error;
 static bool autoclock = true;
-<<<<<<< HEAD
-static int usb_nonswitch_match(struct usb_device* udev);
-=======
+
 static int usb_nonswitch_match(struct usb_device *udev);
->>>>>>> update/master
 
 module_param_array(index, int, NULL, 0444);
 MODULE_PARM_DESC(index, "Index value for the USB audio adapter.");
@@ -778,18 +775,7 @@ static struct usb_device_id usb_audio_ids [] = {
 MODULE_DEVICE_TABLE(usb, usb_audio_ids);
 
 #ifdef CONFIG_SWITCH
-<<<<<<< HEAD
-static struct usb_device_id usb_nonswitch_ids [] = {
-#include "nonswitch-table.h"
-    { }
-};
 
-static int usb_nonswitch_match(struct usb_device* udev) {
-	int i;
-	for(i = 0; i < sizeof(usb_nonswitch_ids); i++) {
-		if ((usb_nonswitch_ids[i].idVendor == udev->descriptor.idVendor) &&
-				(usb_nonswitch_ids[i].idProduct == udev->descriptor.idProduct))
-=======
 static struct usb_device_id usb_nonswitch_ids[] = {
 #include "nonswitch-table.h"
 	{ }
@@ -802,7 +788,6 @@ static int usb_nonswitch_match(struct usb_device *udev)
 		if (
 		(usb_nonswitch_ids[i].idVendor == udev->descriptor.idVendor) &&
 		(usb_nonswitch_ids[i].idProduct == udev->descriptor.idProduct))
->>>>>>> update/master
 			return 1;
 	}
 	return 0;
